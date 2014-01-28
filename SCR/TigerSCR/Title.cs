efc7f2ed9866626397fb65f262d5ea783aba0c5d
RomainDAV <romain.daversa@gmail.com>
2014-01-28 03:44:44 -0800

    Title : inclusion de Données OCDE et UE

--- SCR/TigerSCR/Title.cs
+++ SCR/TigerSCR/Title.cs
@@ -17,6 +17,72 @@ namespace TigerSCR
         private bool ocde;
         private bool ue;
 
+        #region donnees
+        List<string> oecd = new List<string>{
+            "AUSTRALIA",
+            "AUSTRIA",
+            "BELGIUM",
+            "CANADA",
+            "CHILE",
+            "CZECH REPUBLIC",
+            "DENMARK",
+            "ESTONIA",
+            "FINLAND",
+            "FRANCE",
+            "GERMANY",
+            "GREECE",
+            "HUNGARY",
+            "ICELAND",
+            "IRLAND",
+            "ISRAEL",
+            "ITALY",
+            "JAPAN",
+            "KOREA",
+            "LUXEMBOURG",
+            "MEXICO",
+            "NETHERLANDS",
+            "NEW ZEALAND",
+            "NORWAY",
+            "POLAND",
+            "PORTUGAL",
+            "SLOVAKIA",
+            "SLOVENIA",
+            "SPAIN",
+            "SWEDEN",
+            "SWITZERLAND",
+            "TURKEY",
+            "UNITED KINGDOM",
+            "UNITED STATES"};
+        List<string> ue = new List<string>{
+            "AUSTRIA",
+            "BELGIUM",
+            "BULGARIA",
+            "CROATIA",
+            "CYPRUS",
+            "CZECH REPUBLIC",
+            "ESTONIA",
+            "FINLAND",
+            "FRANCE",
+            "GERMANY",
+            "GREECE",
+            "HUNGARY",
+            "IRLAND",
+            "ITALY",
+            "LATVIA",
+            "LITHUANIA",
+            "LUXEMBOURG",
+            "MALTA",
+            "NETHERLANDS",
+            "POLAND",
+            "PORTUGAl",
+            "ROMANIA",
+            "SLOVAKIA",
+            "SLOVENIA",
+            "SPAIN",
+            "SWEDEN",
+            "UNITED KINDGDOM"};
+            #endregion
+
         public Title(string _isin, Security _security, string _countrySecurity)
         {
             this.isin = _isin;
